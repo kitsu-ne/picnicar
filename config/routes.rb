2401637bf3248ac4b7a0e7979c5bbc41af87a71e
hiroonrails <m69lover.scachaaan@gmail.com>
2021-06-26 14:43:34 +0900

    2代目導入完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
   devise_for :owners
   devise_for :users
-
-  devise_for :owners, controllers: {
-    sessions:      'owners/sessions',
-    passwords:     'owner/passwords',
-    registrations: 'owners/registrations'
-  }
-  devise_for :users, controllers: {
-    sessions:      'users/sessions',
-    passwords:     'users/passwords',
-    registrations: 'users/registrations'
-  }
-  
   root to:'picnicars#index'
   resources :picnicars
 end
